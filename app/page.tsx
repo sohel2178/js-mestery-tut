d3b92ca0a5186c049a0ff004059d3902bea37fae
sohel2178 <sohel.ahmed2178@gmail.com>
2024-10-29 18:42:00 +0600

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,10 @@
 
 export default function Home() {
   console.log("Where am I???");
-  return <h1 className="h1-bold">Welcome to JSMestery</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to JSMestery</h1>;
+      <h1 className="h1-bold font-space-grotesk">Welcome to JSMestery</h1>;
+    </>
+  );
 }
